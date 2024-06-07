e3062663e7853cf66619e75e1d6ac3688843d61f
SAMIRA DONFACK <SAMIRA DONFACK@2NCORPORATE.LOCAL>
2024-06-07 10:45:46 +0100

    version2 sam

---
+++ resources/views/sam.blade.php
@@ -0,0 +1,70 @@
+
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar scroll</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <div class="container-fluid">
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                                <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#"><i class="fa fa-facebook"></i>Home</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#"><i class="fa fa-facebook"></i>Link</a>
+                                </li>
+                                
+                                <li class="nav-item">
+                                <a class="nav-link disabled"><i class="fa fa-facebook"></i>Link</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-6 text-end">
+                            <a class="btn btn-outline-success" href="">connexion</a>
+                            <a class="btn btn-outline-primary" href="">inscription</a>
+                        </div>
+                        
+                        
+                    </div>
+                    <div class="row mt-3 ">
+                        <div class="col-12 center">
+                            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll " style="--bs-scroll-height: 100px;">
+                                <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Link
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link disabled">Link</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
